63895e35fada8bb35c76706781cbf544d2901103
Shawn Mikel Hobayan <hobayan.shawnmikel@gmail.com>
2025-03-28 18:11:27 +0800

    pseudo code for title

---
+++ title.py
@@ -0,0 +1,11 @@
+#Pseudo code for my version of title function
+
+#Begin
+#Input: Read a string
+#Split the string into words
+#Loop through each word:
+#a. Convert the first character to uppercase
+#b. Convert the remaining characters to lowercase
+#Join the modified words back into a single string
+#Output the title-cased string
+#End
