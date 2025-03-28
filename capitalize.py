e2646c058f89755c064eb7548377eb3e0fe13dbc
Shawn Mikel Hobayan <hobayan.shawnmikel@gmail.com>
2025-03-28 18:14:09 +0800

    pseudo code for capitalize

---
+++ capitalize.py
@@ -0,0 +1,9 @@
+#Pseudo code for my version of capitalize function
+
+#Begin
+#Input: Read a string
+#Convert the first character to uppercase if it is a letter
+#Convert all remaining characters to lowercase
+#Output the capitalized string
+#End
+
