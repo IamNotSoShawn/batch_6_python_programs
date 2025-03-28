88d0044dbaf26b3168c113d5cfd4314fa5785c6c
Shawn Mikel Hobayan <hobayan.shawnmikel@gmail.com>
2025-03-28 17:08:36 +0800

    pseudo code for ljust

---
+++ l_just.py
@@ -0,0 +1,8 @@
+#Pseudo code for my version of ljust function
+
+#Begin
+#Input: Read a string and the desired width
+#Calculate the number of spaces needed (width - length of string)
+#Concatenate the original string with the required spaces
+#Output the justified string
+#End
