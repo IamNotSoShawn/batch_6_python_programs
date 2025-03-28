b8204051ee52dd8f5b247af7942b9e9428885067
Shawn Mikel Hobayan <hobayan.shawnmikel@gmail.com>
2025-03-28 15:23:45 +0800

    lstrip pseudo code

---
+++ lstrip.py
@@ -0,0 +1,15 @@
+Start
+
+Input: Read a string from the user
+
+Initialize an empty result string
+
+Initialize a flag found_non_space as False
+
+Loop through each character in the string:
+a. If the character is NOT a space, set found_non_space to True
+b. If found_non_space is True, add the character to the result string
+
+Output the result string
+
+End
