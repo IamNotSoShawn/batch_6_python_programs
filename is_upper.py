12b0998ab57f9a450b9233dd9ad8b5d8da1805b6
Shawn Mikel Hobayan <hobayan.shawnmikel@gmail.com>
2025-03-28 16:35:14 +0800

    pseudo code for isuper

---
+++ is_upper.py
@@ -0,0 +1,10 @@
+#Pseudo code for my version of isupper function
+
+
+#Begin
+#Input: Read a string
+#Loop through each character in the string:
+#a. If there is at least one lowercase letter ('a' to 'z'), return False
+#If no lowercase letters are found, return True
+#Output the result
+#End
