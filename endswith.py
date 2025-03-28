99dcc423151c18c4a2134d3c4fa8d3c2bf778ee6
Shawn Mikel Hobayan <hobayan.shawnmikel@gmail.com>
2025-03-28 16:38:06 +0800

    pseudo code for endswith function

---
+++ endswith.py
@@ -0,0 +1,10 @@
+#Pseudo code for my version of endswith function
+
+#Begin
+#Input: Read a string and a suffix to check
+#Extract the last N characters of the string (N is the length of the suffix)
+#Compare the extracted substring with the suffix:
+#If Equal, return True
+#Otherwise, return False
+#Output the result
+#End
