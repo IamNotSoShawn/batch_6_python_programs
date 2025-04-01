e1cd68af22f75a3ee7113f5f5adc0b7af3c73216
Shawn Mikel Hobayan <hobayan.shawnmikel@gmail.com>
2025-04-01 21:06:34 +0800

    Whole endswith code

--- endswith.py
+++ endswith.py
@@ -8,3 +8,19 @@
 #Otherwise, return False
 #Output the result
 #End
+
+def last_word(word, ending):
+    if len(ending) > len(word):  
+        return False
+
+    last_part = word[-len(ending):] 
+    if last_part == ending: 
+        return True
+    else:
+        return False
+    
+text = input("Enter a string: ")
+last = input("Enter the suffix to check: ")
+
+result = last_word(text, last)
+print("Does it end with '" + last + "':", result)
